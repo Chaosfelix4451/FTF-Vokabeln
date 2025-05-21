21ef82684da4712036748bba3cca4fe5cdf55502
feras <hassanferas2109@gmail.com>
2025-05-21 22:31:24 +0200

    Main-Menu

--- src/Settings/SettingsController.java
+++ src/Settings/SettingsController.java
@@ -4,26 +4,35 @@ package Settings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 import Utils.SceneLoader;
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 
-public class SettingsController {
+public class SettingsController implements Initializable {
     /* Need for Stage change over functions include this code block in every Controller Class
     Pls do not delete or change
      */
 
     private Stage stage = null;
+
+    private final Preferences prefs = Preferences.userNodeForPackage(SettingsController.class);
+
     //@Override
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-    ////////////////////////
+
+    /// /////////////////////
 
     @FXML
     private Button button;
@@ -37,4 +46,43 @@ public class SettingsController {
     public void openTrainer(ActionEvent event) {
         SceneLoader.load(stage, "/Trainer/Trainer.fxml");
     }
+
+    @FXML
+    private ChoiceBox<String> vocabModeBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        vocabModeBox.getItems().addAll(
+                "Deutsch zu Englisch",
+                "Englisch zu Deutsch",
+                "Zufällig"
+        );
+
+        String savedMode = prefs.get("vocabMode", "Deutsch zu Englisch");
+        vocabModeBox.setValue(savedMode);
+
+        vocabModeBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            prefs.put("vocabMode", newVal);
+            System.out.println("Modus gespeichert: " + newVal);
+        });
+    }
+
+    @FXML
+    private void handleStart(ActionEvent event) {
+        String auswahl = vocabModeBox.getValue();
+        System.out.println("Ausgewählter Modus: " + auswahl);
+
+        switch (auswahl) {
+            case "Deutsch zu Englisch":
+                // Starte Deutsch->Englisch Modus
+                break;
+            case "Englisch zu Deutsch":
+                // Starte Englisch->Deutsch Modus
+                break;
+            case "Zufällig":
+                // Wähle zufällig einen Modus
+                break;
+        }
+    }
+
 }
