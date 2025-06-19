bcf60327141307ae986d58a6a100fd94ef937ec7
felix <felixx511007@gmail.com>
2025-06-20 00:23:28 +0200

    json

--- src/Trainer/TrainerModel.java
+++ src/Trainer/TrainerModel.java
@@ -1,12 +1,23 @@
 package Trainer;
 
+import java.io.FileReader;
 import java.util.Arrays;
 import java.util.List;
+//program for reading a JSON file
+
+import org.json.JSONArray;
+
+import org.json.JSONObject;
+
+
+
+
 
 /**
  * Das Model hält eine feste Liste von Vokabeln.
  * Diese kann über Getter abgefragt werden.
  */
+
 public class TrainerModel {
     private final List<String> vocabEnglish = Arrays.asList(
             "apple", "banana", "house", "car", "computer",
@@ -37,4 +48,32 @@ public class TrainerModel {
         public String getTranslation(int index) {
             return vocabGerman.get(index);
         }
+}
+
+public class JSON
+
+{
+
+    public static void main(Strings args[])
+
+    {
+
+        // file name is File.json
+
+        Object o = new JSONParser().parse(new FileReader(File.json));
+
+        JSONObject j = (JSONObject) o;
+
+        String Name = (String) j.get(“Name”);
+
+        String College = (String ) j.get(“College”);
+
+
+
+        System.out.println(“Name :” + Name);
+
+        System.out.println(“College :” +College);
+
+    }
+
 }
