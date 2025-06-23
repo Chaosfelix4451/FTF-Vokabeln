f1ac0f20b41568080afc75818e21ad0c97a3d82b
Chaosfelix4451 <46029764+Chaosfelix4451@users.noreply.github.com>
2025-06-23 15:58:26 +0200

    Enable responsive stage sizing

--- src/Utils/SceneLoader/SceneLoader.java
+++ src/Utils/SceneLoader/SceneLoader.java
@@ -63,6 +63,7 @@ public class SceneLoader {
             }
 
             stage.setScene(scene);
+            stage.setMaximized(true); // adjust to current display size
             stage.show();
 
         } catch (IOException e) {
