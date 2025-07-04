7670447b71b165e4fa9cc2053a7192b3c2fe2245
felix <felixx511007@gmail.com>
2025-07-04 18:55:23 +0200

    new ConnectTrainer

--- src/ConnectTrainer/ConnectTrainerController.java
+++ src/ConnectTrainer/ConnectTrainerController.java
@@ -6,6 +6,7 @@ import Utils.StageAwareController;
 import Utils.UserSys.UserSys;
 import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -16,9 +17,7 @@ import javafx.geometry.Pos;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ConnectTrainerController extends StageAwareController {
 
@@ -28,12 +27,14 @@ public class ConnectTrainerController extends StageAwareController {
     @FXML private AnchorPane drawPane;
 
     private final List<Line> lines = new ArrayList<>();
+    private final Map<Line, Label[]> lineEndpoints = new HashMap<>();
     private Line currentLine;
 
     @FXML
     private void initialize() {
         String listId = UserSys.getPreference("vocabFile", "defaultvocab.json");
         String mode = UserSys.getPreference("vocabMode", "Deutsch zu Englisch");
+
         TrainerModel model = new TrainerModel();
         model.LoadJSONtoDataObj(listId);
 
@@ -49,46 +50,64 @@ public class ConnectTrainerController extends StageAwareController {
             // LEFT
             Label vocabLeft = new Label(model.get(id, langPair[0]));
             vocabLeft.getStyleClass().add("vocab-box");
+            VBox.setMargin(vocabLeft, new Insets(5));
 
             Label connLeft = new Label();
-            connLeft.setPrefSize(20, 20);
+            connLeft.setPrefSize(10, 20);
             connLeft.getStyleClass().add("connector-box");
-            connLeft.setTranslateX(-10); // overlap half with label
+            connLeft.setTranslateX(0);
 
             HBox leftRow = new HBox(vocabLeft, connLeft);
             leftRow.setSpacing(0);
             leftRow.setAlignment(Pos.CENTER_RIGHT);
+            VBox.setMargin(leftRow, new Insets(10));
             leftBox.getChildren().add(leftRow);
 
             // RIGHT
             Label connRight = new Label();
             connRight.setPrefSize(20, 20);
             connRight.getStyleClass().add("connector-box");
-            connRight.setTranslateX(10); // overlap half with label
+            connRight.setTranslateX(10);
 
             Label vocabRight = new Label(model.get(id, langPair[1]));
             vocabRight.getStyleClass().add("vocab-box");
+            VBox.setMargin(vocabRight, new Insets(5));
 
             HBox rightRow = new HBox(connRight, vocabRight);
             rightRow.setSpacing(0);
             rightRow.setAlignment(Pos.CENTER_LEFT);
+            VBox.setMargin(rightRow, new Insets(10));
             rightBox.getChildren().add(rightRow);
 
             setupDrag(connLeft);
             setupDrag(connRight);
         }
+
+        // Redraw on pane size change
+        drawPane.widthProperty().addListener((obs, oldVal, newVal) -> redrawAllLines());
+        drawPane.heightProperty().addListener((obs, oldVal, newVal) -> redrawAllLines());
+
+        // Zusätzlich bei Layout-Änderung (z.B. Stage resized)
+        leftBox.layoutBoundsProperty().addListener((obs, o, n) -> redrawAllLines());
+        rightBox.layoutBoundsProperty().addListener((obs, o, n) -> redrawAllLines());
+
+        // Zusätzlich bei Stage-Resize (z. B. Wechsel Vollbild/Fenster)
+        drawPane.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                newScene.windowProperty().addListener((obsWin, oldWin, newWin) -> {
+                    if (newWin instanceof javafx.stage.Stage stage) {
+                        stage.widthProperty().addListener((o, oldW, newW) -> redrawAllLines());
+                        stage.heightProperty().addListener((o, oldH, newH) -> redrawAllLines());
+                    }
+                });
+            }
+        });
+
     }
 
     private void setupDrag(Label connector) {
         connector.setOnMousePressed(e -> {
-            // Remove old line from this connector
-            lines.removeIf(line -> {
-                if (line.getUserData() == connector) {
-                    drawPane.getChildren().remove(line);
-                    return true;
-                }
-                return false;
-            });
+            removeOldLines(connector);
 
             Point2D startPoint = getCenter(connector);
             currentLine = new Line(startPoint.getX(), startPoint.getY(), startPoint.getX(), startPoint.getY());
@@ -108,18 +127,49 @@ public class ConnectTrainerController extends StageAwareController {
         connector.setOnMouseReleased(e -> {
             if (currentLine != null) {
                 Point2D release = new Point2D(e.getSceneX(), e.getSceneY());
-                Label target = findConnectorAt(release, connector);
+                Label target = findConnectorAt(release, (Label) currentLine.getUserData());
                 if (target != null) {
                     Point2D endPoint = getCenter(target);
                     currentLine.setEndX(endPoint.getX());
                     currentLine.setEndY(endPoint.getY());
                     lines.add(currentLine);
+                    lineEndpoints.put(currentLine, new Label[]{(Label) currentLine.getUserData(), target});
                 } else {
                     drawPane.getChildren().remove(currentLine);
                 }
                 currentLine = null;
             }
         });
+    }
+
+    private void removeOldLines(Label connector) {
+        List<Line> toRemove = new ArrayList<>();
+        for (Line l : lines) {
+            Label[] endpoints = lineEndpoints.get(l);
+            if (endpoints != null && (endpoints[0] == connector || endpoints[1] == connector)) {
+                toRemove.add(l);
+            }
+        }
+        for (Line l : toRemove) {
+            drawPane.getChildren().remove(l);
+            lines.remove(l);
+            lineEndpoints.remove(l);
+        }
+    }
+
+    private void redrawAllLines() {
+        for (Map.Entry<Line, Label[]> entry : lineEndpoints.entrySet()) {
+            Line line = entry.getKey();
+            Label[] pair = entry.getValue();
+
+            Point2D p1 = getCenter(pair[0]);
+            Point2D p2 = getCenter(pair[1]);
+
+            line.setStartX(p1.getX());
+            line.setStartY(p1.getY());
+            line.setEndX(p2.getX());
+            line.setEndY(p2.getY());
+        }
     }
 
     private Label findConnectorAt(Point2D scenePoint, Label ignore) {
@@ -140,8 +190,9 @@ public class ConnectTrainerController extends StageAwareController {
 
     private Point2D getCenter(Label node) {
         Bounds bounds = node.localToScene(node.getBoundsInLocal());
-        return new Point2D(bounds.getMinX() + bounds.getWidth() / 2,
+        Point2D scenePoint = new Point2D(bounds.getMinX() + bounds.getWidth() / 2,
                 bounds.getMinY() + bounds.getHeight() / 2);
+        return drawPane.sceneToLocal(scenePoint);
     }
 
     private Point2D sceneToPane(double x, double y) {
